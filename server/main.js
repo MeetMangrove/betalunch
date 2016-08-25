9261f7d98af79ed38e5e887d40dc6a322748a03e
Thomas Jeanneau <thomas@newsource.io>
2016-08-25 18:22:58 +0200

    set date normal

--- server/main.js
+++ server/main.js
@@ -93,14 +93,14 @@ const jobInitialisation = new cron.CronJob({
 });
 
 const jobAskForMatching = new cron.CronJob({
-    cronTime: '00 33 14 * * 1-5',
+    cronTime: '00 00 9 * * 1-5',
     onTick: askForMatching,
     start: false,
     timeZone: timeZone
 });
 
 const jobMatchingMail = new cron.CronJob({
-    cronTime: '00 00 18 * * 1-5',
+    cronTime: '00 00 12 * * 1-5',
     onTick: matchingMail,
     start: false,
     timeZone: timeZone
